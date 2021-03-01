ef85e408d36026f786493b37c9c131891bf9a91c
Miguel Bertaud <mbertaud@brightcove.com>
2021-02-28 19:52:42 -0600

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Property.delete_all
+
+10.times do |i|
+  tenant = Tenant.create(name: "Player#{i}", email: "foo#{i}@bar.com", phone: "#{i}43535664")
+end
+
+Tenant.all.each do |t|
+  t.properties.create(
+    name: Faker::Address.street_name,
+    description: Faker::Address.full_address ,
+    rental_price: Faker::Number.decimal(l_digits: 4, r_digits: 2),
+    status: ["available", "deleted", "published"].sample
+  )
+end
